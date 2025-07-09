9c38baf2a46181da80a04109009c23162aca571f
kuragna <abourrich@proton.me>
2025-07-09 19:32:48 +0200

    add exception

--- src/main/java/com/avaj_launcher/AircraftFactory.java
+++ src/main/java/com/avaj_launcher/AircraftFactory.java
@@ -3,11 +3,9 @@ package com.avaj_launcher;
 public class AircraftFactory {
     // Singleton: https://en.wikipedia.org/wiki/Singleton_pattern
     // Factory Method: https://refactoring.guru/design-patterns/factory-method
-
     private static int id = 1;
     private static AircraftFactory instance = null;
     private AircraftFactory() {}
-
 
     public static AircraftFactory getInstance() {
         if (instance == null) instance = new AircraftFactory();
@@ -21,6 +19,7 @@ public class AircraftFactory {
             case "balloon":    flyable = new Balloon(id, p_name, p_coordinate);    break;
             case "helicopter": flyable = new Helicopter(id, p_name, p_coordinate); break;
             case "jetplane":   flyable = new JetPlane(id, p_name, p_coordinate);   break;
+            default: throw new UnknownAircraftTypeException("Unknown aircraft type: " + p_type);
         }
         id += 1;
         return flyable;
